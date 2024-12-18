babb38fc4d9582895874d3b60cbc64092140c934
tcurtis <realthomascurtis@gmail.com>
2024-12-17 19:28:18 -0500

    Add plusScore helper function to ScoreButton component
    Introduced the plusScore function to handle score incrementation. This prepares the component for future features that may require score management functionality.

--- src/components/ScoreButton.tsx
+++ src/components/ScoreButton.tsx
@@ -1,5 +1,11 @@
 import { Button } from '@mui/material'
 
+function plusScore() {
+  let score = 0;
+  return score + 1;
+}
+
+
 export default function ScoreButton() {
   return <Button
     fullWidth={true}
